7a0ac9cae6fa8e12b0f134511f78d0fc374d7223
Asinus <b.bogataj23@gmail.com>
2022-02-19 19:05:29 +0100

    restored working state, commented out PLL blinker

--- main.c
+++ main.c
@@ -25,10 +25,8 @@
 void PUT32 ( unsigned int, unsigned int );
 unsigned int GET32 ( unsigned int );
 void DUMMY ( unsigned int );
-void LOOP (void);
 void LEDON(void);
 void LEDOFF(void);
-void LOOP(void);
 
 // C functions 
 void blinker ( unsigned int n )
@@ -98,40 +96,43 @@ int main ( void )
     /**************** Blinker with PLL**********************/
     /*******************************************************/
 
-    // set RCC configuration regeister (RCC_CFGR offset 0x04)
-    ra = 0;             // clear register 
-    ra |= 0x7<<24;      // MCO:111 - pll selecte
-    ra |= 0x0<<22;      // USB prescaler 
-    ra |= 0x7<<18;      // PLLMUL je 0111(9) x 8Mhz = 72Mhz
-    ra |= 0x0<<17;      // PLL_XTPRE: 0-HSE, 1-HSE/2
-    ra |= 0x1<<16;      // PLL_SRC: 0-HSI/2, 1-HSE e
-    ra |= 0x0<<14;      // ADCPRE
-    ra |= 0x0<<11;      // PPRE2 // APB2 prescaler not divided 0xx
-    ra |= 0x4<<8;       // PPRE1 // APB1 precaler not divided 0xx
-    ra |= 0x0<<4;       // HPRE // AHB prescaer, Sys clock no divided
-    PUT32(RCC_CFGR,ra); // put back modified val 
+    // // set RCC configuration regeister (RCC_CFGR offset 0x04)
+    // ra = 0;             // clear register 
+    // ra |= 0x7<<24;      // MCO:111 - pll selecte
+    // ra |= 0x0<<22;      // USB prescaler 
+    // ra |= 0x7<<18;      // PLLMUL je 0111(9) x 8Mhz = 72Mhz
+    // ra |= 0x0<<17;      // PLL_XTPRE: 0-HSE, 1-HSE/2
+    // ra |= 0x1<<16;      // PLL_SRC: 0-HSI/2, 1-HSE e
+    // ra |= 0x0<<14;      // ADCPRE
+    // ra |= 0x0<<11;      // PPRE2 // APB2 prescaler not divided 0xx
+    // ra |= 0x4<<8;       // PPRE1 // APB1 precaler not divided 0xx
+    // ra |= 0x0<<4;       // HPRE // AHB prescaer, Sys clock no divided
+    // PUT32(RCC_CFGR,ra); // put back modified val 
 
-    // ResetClockController_ControlRegister 
-    ra = GET32(RCC_CR);                         // get register
-    ra |= 1<<24;                                // turn on PLL (bit PLLON)
-    PUT32(RCC_CR,ra);                           // write back modified value
-    while(1) if(GET32(RCC_CR)&(1<<25)) break;   // wait for ready flag (bit 25 PLLRDY)
+    // // ResetClockController_ControlRegister 
+    // ra = GET32(RCC_CR);                         // get register
+    // ra |= 1<<24;                                // turn on PLL (bit PLLON)
+    // PUT32(RCC_CR,ra);                           // write back modified value
+    // while(1) if(GET32(RCC_CR)&(1<<25)) break;   // wait for ready flag (bit 25 PLLRDY)
 
-    PUT32(FLASH_ACR,0x2);                       // flash memory interface 0x40022000-0x400223FF 
 
-    ra = GET32(RCC_CFGR);                           // get register 
-    ra &= ~(0x3<<0);                                // clear bits [1:0] (system clock switch)
-    ra |= (0x2<<0);                                 // set bit [1] PLL selected as system clock
-    PUT32(RCC_CFGR,ra);                             // put back modified value  
-    while(1) if((GET32(RCC_CFGR)&0xF)==0xA) break;  // wait for 2 flags, bit [3] - PLL used as sysclk (switch status set by hw)
-                                                    //                   bit [1] - PLL selected as sysclk (switch set by hw, sets HSI if HSE breaks)
+
+    // // Flash access control register, defines latency in clock_cycles for reading flash
+    // PUT32(FLASH_ACR,0x4);                       // flash memory interface 0x40022000-0x400223FF 
+
+    // ra = GET32(RCC_CFGR);                           // get register 
+    // ra &= ~(0x3<<0);                                // clear bits [1:0] (system clock switch)
+    // ra |= (0x2<<0);                                 // set bit [1] PLL selected as system clock
+    // PUT32(RCC_CFGR,ra);                             // put back modified value  
+    // while(1) if((GET32(RCC_CFGR)&0xF)==0xA) break;  // wait for 2 flags, bit [3] - PLL used as sysclk (switch status set by hw)
+    //                                                 //                   bit [1] - PLL selected as sysclk (switch set by hw, sets HSI if HSE breaks)
     
-    // blink with frequency determined by PLL
-    blinker(30000);
+    // // blink with frequency determined by PLL
+    // blinker(30000);
 
 
 
-    /*
+    
     int j = 3;
     int i = 2000000;
     while(j--){
@@ -142,7 +143,7 @@ int main ( void )
         i = 2000000;
         while(i--) DUMMY(i);
     }
-    */
+    
  
     
     return(0);
--- startup.s
+++ startup.s
@@ -1,24 +1,53 @@
+/* both .global and .globl are accepted for compatibility */
+
 .thumb
 .thumb_func
+
 .global _start
 _start:
+
 stacktop: .word 0x20001000
+
 .word reset
 .thumb_func
 reset:
     bl main
     b .
+
+
 .thumb_func
 .globl PUT32
 PUT32:
     str r1,[r0]
     bx lr
+
+
 .thumb_func
 .globl GET32
 GET32:
     ldr r0,[r0]
     bx lr
+
+
 .thumb_func
 .globl DUMMY
 DUMMY:
+    bx lr
+
+
+.thumb_func
+.globl LEDON
+LEDON:
+    ldr r1, =0x40011010 
+    ldr r3, =0x2000
+    str r3, [r1]
+    bx lr
+
+
+.thumb_func
+.globl LEDOFF
+LEDOFF: 
+    ldr r1, =0x40011010 
+    ldr r3, =0x20000000
+    str r3, [r1]
     bx lr
